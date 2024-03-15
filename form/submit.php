cc8c6d3fa45f7108971fbb6b2d34bd8c840c5ebd
mikechekletsov <chekletsovmv@gmail.com>
2024-03-15 17:13:25 +0200

    Add 'form/' from commit '02875048e319d44b7e8c2c14c02b7617150ee31d'
    git-subtree-dir: form
    git-subtree-mainline: 02875048e319d44b7e8c2c14c02b7617150ee31d
    git-subtree-split: 02875048e319d44b7e8c2c14c02b7617150ee31d

---
+++ form/submit.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+$username = htmlspecialchars(trim($_POST["username"]));
+$email = htmlspecialchars(trim($_POST["email"]));
+$message = htmlspecialchars(trim($_POST["message"]));
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Неверный формат email.");
+}
+
+$file_path = '';
+if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+    $file_tmp_path = $_FILES['file']['tmp_name'];
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_type = $_FILES['file']['type'];
+    $allowed_types = array('image/png', 'image/jpeg');
+
+    if (!is_dir("uploads")) {
+        mkdir("uploads", 0775, true);
+    }
+
+    if (in_array($file_type, $allowed_types)) {
+        $upload_path = 'uploads/';
+        $file_path = $upload_path . $file_name;
+        move_uploaded_file($file_tmp_path, $file_path);
+    } else {
+        die("Недопустимый формат файла.");
+    }
+}
+
+$filename = "uploads/messages/" . time() . ".txt";
+if (!is_dir("uploads/messages")) {
+    mkdir("uploads/messages", 0775, true);
+}
+$file_content = "Имя пользователя: $username\nE-mail: $email\nСообщение: $message\nФайл: " . __DIR__ . "/" . $file_path . "\n";
+file_put_contents($filename, $file_content);
+
+$_SESSION['success_message'] = "Ваше сообщение успешно отправлено!";
+
+header("Location: index.php");
+exit;
